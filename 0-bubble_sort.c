4d14585b0315f8b79048bc12763c71ddb7ef6bb9
LovethAwusi <awusieloho@gmail.com>
2023-12-19 11:00:54 -0800

    Redid

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,32 +1,52 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order,
- * using the Bubble sort algorithm
- * @array: the array
- * @size: size of the array
-**/
+ * swap - Swap 2 integers
+ * @x: The first integer
+ * @y: The second integer
+ *
+ * Return: Void or nothing
+ */
+
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascendingorder
+ * using bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: Nothing / void
+ */
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, n = size;
-	int temp;
-	int new;
+	size_t i, j;
+	int can_swap;
 
-	do {
-		new = 0;
+	if (array == NULL || size < 2)
+		return;
 
-		for (i = 1; i < n; i++)
+	for (i = 0; i < size - 1; i++)
+	{
+		can_swap = 0;
+		/* Keep reducing the number of iteration */
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i - 1];
-				array[i - 1] = array[i];
-				array[i] = temp;
-				new = i;
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				can_swap = 1;
 			}
 		}
-		n = new;
-	} while (n >= 1);
+	/* Break if the array has been totally sorted b4 the num of iteration*/
+		if (!can_swap)
+			break;
+	}
 }
